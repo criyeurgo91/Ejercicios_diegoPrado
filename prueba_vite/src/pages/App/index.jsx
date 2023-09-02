b892e0e94fa735453c15259014597eed06d76fee
cristianUrbano <criurbano316@hotmail.com>
2023-09-01 20:27:44 -0500

    actualizacion1

---
+++ prueba_vite/src/pages/App/index.jsx
@@ -0,0 +1,29 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'    
+import { CartContextProvider } from '../../Context'
+import Home from '../Home'
+import Contact from '../Contact'
+import NotFound from '../NotFound'
+import Navbar from '../../componentes/Navbar'
+
+// Declaración Rutas
+const AppRoutes = () => {
+    let routes = useRoutes([
+      {path: '/', element: <Home/>},
+      {path: '/contacto', element: <Contact/>},
+      {path: '/*', element: <NotFound/>}
+    ])  
+    return routes
+}
+
+const App = () => {
+  return (
+    <CartContextProvider>
+        <BrowserRouter>
+            <AppRoutes/>
+            <Navbar/>
+        </BrowserRouter>
+    </CartContextProvider>
+
+)
+}
+export default App
---
+++ prueba_vite/src/pages/Contact/index.jsx
@@ -0,0 +1,13 @@
+import Layout from '../../componentes/Layout'
+
+function Contact() {
+  return (
+    <>
+      <Layout>
+        Contacto
+      </Layout>
+    </>
+  )
+}
+
+export default Contact
